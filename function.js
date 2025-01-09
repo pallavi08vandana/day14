f11b815f61baeab5e9fc51ce08377e8d05db50d7
pallavi <2211cs010589@mallareddyuniversity.ac.in>
2025-01-09 11:48:51 +0530

    pushing into the repo

---
+++ function.js
@@ -0,0 +1,49 @@
+// //! function without parametrs
+// function demo(){
+//     console.log("function wothout parameters");
+// }
+// demo();
+//! function with parameters
+// function userDetails(name, age, city){
+//     console.log(name);
+//     console.log(age);
+//     console.log(city);
+
+// }
+// userDetails("pallavi", 19, "Hyderabad");
+
+// //! anonymus function
+// function(){
+//     console.log("Anonymus function");
+// }
+
+//! function expression
+// let x=function() {
+//     console.log("function expression is executing");
+// }
+// x();
+
+//! IIFE
+// (function(){
+//     console.log("IIFE")
+// })
+// ();
+//! Arrow function
+// function demo(){
+//     console.log("Hello")
+// }
+// demo();
+
+// let x =_=>console.log("Arrow function");
+// x(5);
+
+// let x =(a, b)=>console.log(a+b);
+// x(5,5);
+
+// let x=_=>{
+//     console.log("Hi");
+//         console.log("Hello");
+//         console.log("Bye");
+
+// }
+// x();
---
+++ loopin.js
@@ -0,0 +1,38 @@
+//!for loop
+// for(let i=1;i<=5;i++){
+//     console.log("Good morning");
+// }
+
+// for(let i=1;i<=100;i++){
+//     if(i%2==0){
+//         console.log(i+"is a even number");
+//     }
+//     else{
+//         console.log(i+"is a odd number");
+//     }
+// }
+
+//!while loop
+// let i=1;
+// while(i<=5){
+    
+//     i++;
+//     console.log("good moring");
+// }
+
+// let i=1;
+// let n=5;
+// do{
+//     i++;
+//     console.log("good moring");
+// }while(i>=n)
+
+// for(let i=1;i<=10;i++){
+//     if(i==5)continue;
+//     console.log(i);
+       
+// }
+
+// function addToCart(){
+//     alert("Product is added to cart");
+// }
