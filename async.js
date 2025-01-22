4db924515ca1167ca505279c454c4f774581c126
pallavi <2211cs010589@mallareddyuniversity.ac.in>
2025-01-22 20:42:17 +0530

    pushing into the repo

--- async.js
+++ async.js
@@ -26,3 +26,36 @@ let p3=new Promise((resolve,reject)=>{
 
 })
 console.log(p3);
+
+//! API fetching
+
+//! https://jsonplaceholder.typicode.com/users
+
+function fetchUsers()
+{
+    let repsonse = fetch("https://jsonplaceholder.typicode.com/users");
+    //console.log(response);
+    repsonse.then(result=>{
+        //console.log(result.json());
+        return result.json().then(data=>{
+            console.log(data);
+            let store = document.getElementById("store");
+            console.log(store);
+            data.map(user=>{
+                store.innerHTML += `
+                <tr>
+                <td>${user.id}</td>
+                <td>${user.name}</td>
+                <td>${user.email}</td>
+                <td>${user.company.name}</td>
+                </tr>
+    
+                `
+                
+            })
+        })
+
+    })
+    .catch(Err=>console.log(err))
+}
+fetchUsers();
